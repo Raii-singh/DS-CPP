8c14536d12d878698c2f8f53b235998646497111
Rai Singh <150220305+Raii-singh@users.noreply.github.com>
2026-05-21 23:46:13 +0530

    Time: 0 ms (100%), Space: 20.7 MB (57.53%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++){
-            bool ans = false;
-            for(int j = 0; j < nums.size(); j++){
-                if(nums[i] == nums[j] && i != j){
-                    ans = true;
-                }
-            }
-            if(ans == false) return nums[i];
+        int test=0;
+        for(int val : nums){
+            test ^=val;
+
         }
-        return -1;
+        return test;
     }
 };
