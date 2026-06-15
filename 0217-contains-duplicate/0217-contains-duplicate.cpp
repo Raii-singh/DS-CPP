c0991167f9e3fe08519088bbac729d75a480afb6
Rai Singh <150220305+Raii-singh@users.noreply.github.com>
2026-06-16 02:27:14 +0530

    Time: 47 ms (68.72%), Space: 90.9 MB (27.72%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> ans;
+        for(int i=0; i<nums.size(); i++){
+            ans[nums[i]] += 1;
+            if(ans[nums[i]] >= 2) return true;
+        }
+        return false;
+    }
+};
