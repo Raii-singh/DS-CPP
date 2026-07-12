22332f4df82dc7fcbd9ccf2ac65943730d5e231a
Rai Singh <150220305+Raii-singh@users.noreply.github.com>
2026-07-12 15:31:49 +0530

    Time: 42 ms (61.26%) , Array

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int ans = 0;
+        
+        int count = 0;
+        int sum = 0;
         for(int i = 0; i < nums.size(); i++){
-            int sum = 0;
-            for(int j = i; j < nums.size(); j++){
-                sum += nums[j];
-                if(sum == k) ans++;
-            }
+            nums[i] += sum;
+            sum = nums[i];
         }
-        return ans;
+        unordered_map<int,int> mp;
+
+        mp[0] = 1;
+        for(int i = 0; i < nums.size(); i++){
+            int need = nums[i] - k;
+            if(mp[need]){
+                count += mp[need];
+            }
+            mp[nums[i]]++;
+        }
+        return count;
     }
 };
