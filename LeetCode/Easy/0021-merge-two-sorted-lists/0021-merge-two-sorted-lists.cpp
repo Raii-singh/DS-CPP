f17932fad0a26d8c3aa09936cc2d0c9390adba2b
Rai Singh <150220305+Raii-singh@users.noreply.github.com>
2026-07-13 14:41:29 +0530

    Time: 0 ms (100%) , Linked List

---
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* ans = new ListNode();
+        ListNode* merged = ans;
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val > list2->val){
+                merged->next = list2;
+                list2 = list2->next;
+                merged = merged->next;
+            }
+            else{
+                merged->next = list1;
+                list1 = list1->next;
+                merged = merged->next;
+            }
+        }
+        while(list1 != NULL){
+            merged->next = list1;
+            list1 = list1->next;
+            merged = merged->next;
+        }
+        while(list2 != NULL){
+            merged->next = list2;
+            list2 = list2->next;
+            merged = merged->next;
+        }
+        return ans->next;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
