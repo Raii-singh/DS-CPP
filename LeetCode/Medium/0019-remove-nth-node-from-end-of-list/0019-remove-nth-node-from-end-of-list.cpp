58b8f5ca5f2f46b49e77ae12ece4a37f760cdd03
Rai Singh <150220305+Raii-singh@users.noreply.github.com>
2026-07-14 01:16:15 +0530

    Time: 0 ms (100%) , Linked List

--- LeetCode/Medium/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ LeetCode/Medium/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,39 +11,19 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp = new ListNode();
-        temp -> next = head;
-        int count = 0;
-        while(temp->next != nullptr){
-            count++;
-            temp = temp->next;
+        ListNode* ans = new ListNode();
+        ans->next = head;
+        ListNode* slow = ans;
+        ListNode* fast = ans;
+                
+        for(int i = 0; i <= n; i++)
+            fast = fast->next;
+        while(fast != nullptr){
+            fast = fast->next;
+            slow = slow->next;
         }
+        if(slow->next != nullptr) slow->next = slow->next->next;
 
-        ListNode* temp1 = new ListNode();
-        temp1 -> next = head;
-        ListNode* ans = temp1;
-        int count1 = 0;
-        while(temp1->next != nullptr){
-            if(count1 == count - n){
-                temp1->next = temp1->next->next;
-                count1++;
-                break;
-            }
-            temp1 = temp1->next;
-            count1++;
-        }
         return ans->next;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
