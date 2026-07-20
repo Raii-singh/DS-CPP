2b1d0b5feafc40ef2a7d979126c5c20b9a61e63e
Rai Singh <150220305+Raii-singh@users.noreply.github.com>
2026-07-20 15:39:04 +0530

    Time: 0 ms (100%) , Two Pointers

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.cpp
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int i = 0;
-        int j = s.size() - 1;
 
-        while(i < j){
-            swap(s[i], s[j]);
-            i++;
-            j--;
-        }       
+    void fx(vector<char>&s ,int left,int right){
+        if(left >= right) return;
+        swap(s[left], s[right]);
+        return fx(s, left+1, right-1);
+    }
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size()-1;
+        fx(s, left, right);
     }
 };
