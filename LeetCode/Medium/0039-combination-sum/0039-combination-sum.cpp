9a5b40c7587a88b523e32fed5b137a5d14d1aad8
Rai Singh <150220305+Raii-singh@users.noreply.github.com>
2026-07-23 14:38:40 +0530

    Time: 0 ms (100%) , Array

--- LeetCode/Medium/0039-combination-sum/0039-combination-sum.cpp
+++ LeetCode/Medium/0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void fx(int idx, vector<int>& nums, vector<vector<int>>& ans
-    , vector<int> candidates, int target){
+    void fx(int idx, vector<int>& candidates, vector<int>& sub, vector<vector<int>>& dom, int target){
         if(target < 0 || idx == candidates.size()) return;
         if(target == 0){
-            ans.push_back(nums);
+            dom.push_back(sub);
             return;
         }
-        nums.push_back(candidates[idx]);
-        fx(idx, nums, ans, candidates, target - candidates[idx]);
 
-        nums.pop_back();
-        fx(idx + 1, nums, ans, candidates, target);
-
+        sub.push_back(candidates[idx]);
+        fx(idx, candidates, sub, dom, target - candidates[idx]);
+        
+        sub.pop_back();
+        fx(idx + 1, candidates, sub, dom, target);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> nums;
-        vector<vector<int>> ans;
-        fx(0, nums, ans, candidates, target);
-        return ans;
+        vector<int> sub;
+        vector<vector<int>> dom;
+
+        fx(0, candidates, sub, dom, target);
+        return dom;
     }
 };
