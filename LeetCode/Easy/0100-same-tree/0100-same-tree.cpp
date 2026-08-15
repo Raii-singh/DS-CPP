68292e8caa4d281dd1ac05b44af995e3fd1811b0
Rai Singh <150220305+Raii-singh@users.noreply.github.com>
2026-08-16 00:00:25 +0530

    Time: 0 ms (100%) , Tree

--- LeetCode/Easy/0100-same-tree/0100-same-tree.cpp
+++ LeetCode/Easy/0100-same-tree/0100-same-tree.cpp
@@ -12,9 +12,7 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == nullptr && q == nullptr) return true;
-        else if(p != nullptr && q != nullptr){}
-        else return false;
+        if(p == nullptr || q == nullptr) return p == q;
 
         if(p->val != q->val) return false;
 
